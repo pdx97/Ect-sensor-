d4ec215044c99050c17204e19473f9bfe7daae3c
pdx97 <prakhhar1997@gmail.com>
2017-07-24 22:59:33 +0530

    Add files via upload

---
+++ ect2.ino
@@ -0,0 +1,44 @@
+const int lpin =18;
+const int rpin=11;
+int val=0;
+int preval=0;
+
+void setup() {
+  
+pinMode(18,INPUT);
+pinMode(11,OUTPUT);
+
+}
+
+void loop() {
+  
+  val=analogRead(lpin);
+  
+   if(val-preval>0)
+   
+   { if(val>0.8)
+   
+   digitalWrite(rpin,HIGH);
+   
+   else
+   
+   digitalWrite(rpin,LOW);
+   }
+
+ if(val-preval<0)
+ 
+   { if(val<0.7)
+   
+   digitalWrite(rpin,LOW);
+   
+   else
+   
+   digitalWrite(rpin,HIGH);
+   }
+
+   preval=val;
+   
+   val=0;
+
+}
+
